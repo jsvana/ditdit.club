7daee07dd7adad05236dfd2fd61ab57a152f8400
Jay Vana <jaysvana@gmail.com>
2026-01-16 20:06:09 -0800

    Fix workflow to assemble site before upload

--- prop-src/vite.config.js
+++ prop-src/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +9,7 @@ export default defineConfig({
   },
   base: '/prop/',
   build: {
-    outDir: resolve(__dirname, '../prop'),
+    outDir: '../prop',
     emptyOutDir: true,
     sourcemap: false
   }
